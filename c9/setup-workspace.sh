c44dbf482f3a9300dd208bd4e5841426a2be634c
Stephen Hazleton <shazleto@gmail.com>
2017-01-31 05:25:47 +0000

    removing c9 metadata, adding ionic.conf

--- c9/setup-workspace.sh
+++ c9/setup-workspace.sh
@@ -6,9 +6,6 @@ nvm install 6.9.4
 nvm use 6.9.4
 echo "6" > ~/.nvm/alias/default
 
-echo "copying workspace project settings.."
-cp ./project.settings ../.c9
-
 echo "installing node dependencies, go make a cup of tea.."
 cd ~/workspace
 npm install -g cordova@6.0.x ionic && npm install
@@ -16,5 +13,9 @@ npm install -g cordova@6.0.x ionic && npm install
 echo "switching to lesson-one.."
 git checkout lesson-one
 
+echo "copying global ionic.config"
+mkdir -p ~/.ionic
+cp c9/ionic.config ~/.ionic
+
 echo "workspace setup complete.." 
-echo "Don't forget to set the student's Ionic App Name and ID in ~/workspace/ionic.config.json"
+echo "Don't forget to do the remaining manual tasks (README.md)"
