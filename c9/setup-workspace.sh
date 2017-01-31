cc16aa7e469437d49f6228afa21194b0b9c9cec4
Stephen Hazleton <shazleto@gmail.com>
2017-01-31 04:39:06 +0000

    c9 script

--- c9/setup-workspace.sh
+++ c9/setup-workspace.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 
-# setup node
+echo "installing node 6.9.4.."
+source ~/.nvm/nvm.sh
 nvm install 6.9.4
 nvm use 6.9.4
 echo "6" > ~/.nvm/alias/default
 
-# set default project settings / runners
-cp ./project.settings ~/workspace
+echo "copying workspace project settings.."
+cp ./project.settings ../.c9
 
-# install dependencies
+echo "installing node dependencies, go make a cup of tea.."
 cd ~/workspace
 npm install -g cordova@6.0.x ionic && npm install
 
-# checkout lesson one
+echo "switching to lesson-one.."
 git checkout lesson-one
 
+echo "workspace setup complete.." 
 echo "Don't forget to set the student's Ionic App Name and ID in ~/workspace/ionic.config.json"
